dd5022c5aeda09142a424a5999c280f88008ae9b
Viktor Grom <viktorgrom@Viktors-MacBook-Air.local>
2025-09-28 23:27:07 -0700

    Consolidate API functions to stay within Vercel Hobby plan limits
    - Merge all API endpoints into single api/index.js function
    - Remove individual API files to reduce function count
    - Maintain all functionality: auth, trades, statistics, admin
    - Stay within 12 function limit for Hobby plan

--- api/prisma.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate';
-
-// Create Prisma client with Accelerate extension
-const prisma = new PrismaClient().$extends(withAccelerate());
-
-export default prisma;
